f5f60dc8301302d2a23d1585b8707709e2b57a04
Ros <24ftt1076@student.pb.edu.bn>
2025-09-18 07:44:28 +0800

    coloured and changed some text

--- 24ftt1076_asg1/resources/views/home.blade.php
+++ 24ftt1076_asg1/resources/views/home.blade.php
@@ -6,18 +6,15 @@
     <title>Homepage</title>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body class="bg-gray-100 text-gray-900">
+<body class="bg-orange-100 text-black-900">
 <header class="bg-white shadow-md py-4">
     <div class="container mx-auto flex justify-between items-center px-6">
-        <a href="/"><h1 class="text-xl font-bold">Ros's test page</h1></a>
+        <a href="/"><h1 class="text-xl font-bold", style= "color: purple;">HeyThere!</h1></a>
     </div>
 </header>
 <main class="container mx-auto mt-10 px-6 text-center">
-    <h2 class="text-3xl font-bold">Welcome to My Page</h2>
-    <p class="mt-4 text-lg text-gray-600">This is a work in progress, currently thinking</p>
+    <h2 class="text-3xl font-bold", style= "color: #6053C9;">Leave a Message!</h2>
+    <p class="mt-4 text-lg text-gray-600">Add an anonymous note here, just a simple hi or hey will do.</p>
 </main>
-<footer class="mt-10 py-6 bg-white text-center shadow-md">
-    <p class="text-gray-600">&copy; pray i dont break down</p>
-</footer>
 </body>
 </html>
