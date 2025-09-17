36cd6e8f24b9b3cf301919eb9321b8635279cb06
Ros <24ftt1076@student.pb.edu.bn>
2025-09-18 04:39:25 +0800

    added new route

--- 24ftt1076_asg1/routes/web.php
+++ 24ftt1076_asg1/routes/web.php
@@ -7,6 +7,10 @@ Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
+Route::get('/second', function () {
+    return view('second');
+})->name('second');
+
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
